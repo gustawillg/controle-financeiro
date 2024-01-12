295720cba97d6e84f6b0bdf5d05b0d8ce2752208
Gustavo <coartem2@proton.me>
2024-01-12 16:34:40 -0300

    style: mudanças na navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,12 +11,12 @@ const navigation = [
 </script>
 
 <template>
-  <Disclosure v-slot="{ open }" as="nav" class="bg-gray-800">
+  <Disclosure v-slot="{ open }" as="nav" class="bg-transparent">
     <div class="mx-auto max-w-7xl px-2 lg:px-8 sm:px-6">
       <div class="relative h-16 flex items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button -->
-          <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-inset">
+          <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-blue-400 hover:bg-blue-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-inset">
             <span class="absolute -inset-0.5" />
             <span class="sr-only">Open main menu</span>
             <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
@@ -29,12 +29,12 @@ const navigation = [
           </div>
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
-              <a v-for="item in navigation" :key="item.name" :href="item.href" class="rounded-md px-3 py-2 text-sm font-medium" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+              <a v-for="item in navigation" :key="item.name" :href="item.href" class="rounded-md px-3 py-2 text-sm font-medium" :class="[item.current ? 'bg-blue-800 text-white' : 'text-black hover:bg-blue-700 hover:text-white']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
             </div>
           </div>
         </div>
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white focus:ring-offset-gray-800">
+          <button type="button" class="relative rounded-full bg-blue-800 p-1 text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white focus:ring-offset-blue-800">
             <span class="absolute -inset-1.5" />
             <span class="sr-only">View notifications</span>
             <BellIcon class="h-6 w-6" aria-hidden="true" />
@@ -52,13 +52,13 @@ const navigation = [
             <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
               <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <MenuItem v-slot="{ active }">
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700" :class="[active ? 'bg-gray-100' : '']">Your Profile</a>
+                  <a href="#" class="block px-4 py-2 text-sm text-black" :class="[active ? 'bg-gray-100' : '']">Your Profile</a>
                 </MenuItem>
                 <MenuItem v-slot="{ active }">
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700" :class="[active ? 'bg-gray-100' : '']">Settings</a>
+                  <a href="#" class="block px-4 py-2 text-sm text-black" :class="[active ? 'bg-gray-100' : '']">Settings</a>
                 </MenuItem>
                 <MenuItem v-slot="{ active }">
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700" :class="[active ? 'bg-gray-100' : '']">Sign out</a>
+                  <a href="#" class="block px-4 py-2 text-sm text-black" :class="[active ? 'bg-gray-100' : '']">Sign out</a>
                 </MenuItem>
               </MenuItems>
             </transition>
@@ -69,7 +69,7 @@ const navigation = [
 
     <DisclosurePanel class="sm:hidden">
       <div class="px-2 pb-3 pt-2 space-y-1">
-        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white']" :aria-current="item.current ? 'page' : undefined">
+        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium" :class="[item.current ? 'bg-blue-800 text-white' : 'text-black hover:bg-blue-700 hover:text-white']" :aria-current="item.current ? 'page' : undefined">
           {{ item.name }}
         </DisclosureButton>
       </div>
