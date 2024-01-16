5c96e247250a2f3a3e7b82302597c57297b59619
Gustavo <coartem2@proton.me>
2024-01-16 17:28:41 -0300

    lol

--- backend/src/scripts/seedDatabase.js
+++ backend/src/scripts/seedDatabase.js
@@ -1,33 +1,33 @@
-// seedDatabase.ts
-import mongoose from 'mongoose';
-import TransactionModel from '../models/TransactionModel';
+// seedDatabase.js
+const mongoose = require('mongoose');
+const TransactionModel = require('../models/TransactionModel');
 
 async function seedDatabase() {
   try {
-    // Conecte-se ao banco de dados MongoDB
+    // Connect to the MongoDB database
     await mongoose.connect('mongodb://localhost:27017/controle-financeiro', {
-      useNewUrlparser: true,
+      useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     
-    // Crie uma nova transação
+    // Create a new transaction
     const newTransaction = new TransactionModel({
       description: 'Compra de mantimentos',
       amount: -50,
       type: 'despesa',
     });
 
-    // Salve a transação no banco de dados
+    // Save the transaction to the database
     await newTransaction.save();
     
     console.log('Transação salva com sucesso!');
   } catch (error) {
     console.error('Erro ao conectar ou salvar transação:', error);
   } finally {
-    // Desconecte-se do banco de dados após a conclusão
+    // Disconnect from the database after completion
     await mongoose.disconnect();
   }
 }
 
-// Execute a função para popular o banco de dados
+// Execute the function to populate the database
 seedDatabase();
