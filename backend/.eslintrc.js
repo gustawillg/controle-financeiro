3248ad72b190d0eced783e9c0866f354a459e090
Gustavo <coartem2@proton.me>
2024-01-17 23:31:54 -0300

    feat: eslintrc

---
+++ backend/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: 'eslint:recommended',
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module"
+    },
+    env: {
+      node: true,
+      es2021: true,
+      es6:true
+    },
+  };
+  
