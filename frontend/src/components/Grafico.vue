1f9248d746d4c72cdd33b576d9a0402700e3f61d
Gustavo <coartem2@proton.me>
2024-01-23 17:54:58 -0300

    Update Grafico.vue

--- frontend/src/components/Grafico.vue
+++ frontend/src/components/Grafico.vue
@@ -1,21 +1,16 @@
 <template>
     <div>
-      <line-chart :data="chartData" :width="400" :height="200"></line-chart>
+      <line-chart :data="chartData" :options="chartOptions"></line-chart>
     </div>
   </template>
   
   <script>
-  export default {
-    data() {
-      return {
-        chartData: {
-          '2022-01-01': 5,
-          '2022-01-02': 8,
-          '2022-01-03': 3,
-          // Adapte para seus dados reais
-        },
-      };
-    },
-  };
+import VueChartkick from 'vue-chartkick'
+import 'chartkick/chart.js'
+
+app.use(VueChartkick)
+
   </script>
+  
+
   
